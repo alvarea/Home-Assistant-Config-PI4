36684d55d3ca3eb83dac58ce540a1ddf18728563
alvarea <agustin.alvarezconesa@gmail.com>
2018-04-12 07:38:17 +0200

    - Ajustes menores reglas y scripts

--- countpeople.py
+++ countpeople.py
@@ -1,15 +1,10 @@
 home = 0
-for entity_id in hass.states.entity_ids('device_tracker'):
+for entity_id in hass.states.entity_ids('input_boolean'):
     state = hass.states.get(entity_id)
-    if((state.object_id == '93e3d52336a54d4d898b204abdf3ee94' or
-        state.object_id == 'b8b52a3bf0384e72a268705d9b06e496' or
-        state.object_id == 'bef743fa74f2464ba458612ad3e1c195' or
-        state.object_id == 'e3c67d6b51bf445ba241a0045bfe0222') and
-        state.state == 'home'):
-            home = home + 1
+    if(state.object_id in ['home_aac','home_mfc','home_paf','home_aaf'] and state.state == 'on'):
+        home = home + 1
 
 hass.states.set('sensor.family_home', home, {
     'unit_of_measurement': 'personas',
     'friendly_name': '¿Gente en Casa?'
 })
-
