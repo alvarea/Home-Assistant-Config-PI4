e8f3ab7e4a84c993095c8daa488bd1a886d5fd65
alvarea <agustin.alvarezconesa@gmail.com>
2018-04-18 11:59:10 +0200

    - Nueva version Meta-Tracking - Se quitan reglas antiguas de tracking x 4 - Nuevos sensores any_home y all_home para automatizacion

--- countpeople.py
+++ countpeople.py
@@ -1,7 +1,7 @@
 home = 0
-for entity_id in hass.states.entity_ids('input_boolean'):
+for entity_id in hass.states.entity_ids('binary_sensor'):
     state = hass.states.get(entity_id)
-    if(state.object_id in ['home_aac','home_mfc','home_paf','home_aaf'] and state.state == 'on'):
+    if(state.object_id in ['family_home_mfc','family_home_aac','family_home_paf','family_home_aaf'] and state.state == 'on'):
         home = home + 1
 
 hass.states.set('sensor.family_home', home, {
--- meta_device_tracker.py
+++ meta_device_tracker.py
@@ -24,6 +24,13 @@ AAC_Trackers = ['device_tracker.aac_iphone_nmap',
                 'device_tracker.aac_ios',
                 'device_tracker.b8b52a3bf0384e72a268705d9b06e496']
 
+PAF_Trackers = ['device_tracker.paf_iphone_nmap',
+                'device_tracker.93e3d52336a54d4d898b204abdf3ee94']
+
+AAF_Trackers = ['device_tracker.aaf_iphone_nmap',
+                'device_tracker.bef743fa74f2464ba458612ad3e1c195']
+
+
 # Get the entity that triggered the automation
 triggeredEntity = data.get('entity_id')
 
@@ -36,9 +43,18 @@ elif triggeredEntity in MFC_Trackers:
     newFriendlyName = 'Malen Tracker'
     newEntityPicture = '/local/MFC_2017_0.jpeg'
     metatrackerName = 'device_tracker.meta_mfc'
+elif triggeredEntity in PAF_Trackers:
+    newFriendlyName = 'Paloma Tracker'
+    newEntityPicture = '/local/PAF_2017_0.png'
+    metatrackerName = 'device_tracker.meta_paf'
+elif triggeredEntity in AAF_Trackers:
+    newFriendlyName = 'Agu Tracker'
+    newEntityPicture = '/local/AAF_2017_0.png'
+    metatrackerName = 'device_tracker.meta_aaf'
 else:
     newFriendlyName = None
     metatrackerName = None
+    newEntityPicture = None
 
 # Get current & new state
 newState = hass.states.get(triggeredEntity)
@@ -51,6 +67,7 @@ if newState.state is not None:
     newStatus = newState.state
 else:
     newStatus = currentState.state
+
 
 # Create device_tracker.meta entity
 hass.states.set(metatrackerName, newStatus, {
