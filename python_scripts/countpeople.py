ba1c7ddce2eb85ccfe63f52f43debf003fd111c8
alvarea <agustin.alvarezconesa@gmail.com>
2018-06-25 23:31:59 +0200

    - Subida a github /config_esphomeyaml

--- python_scripts/countpeople.py
+++ python_scripts/countpeople.py
@@ -1,10 +1,22 @@
 home = 0
 for entity_id in hass.states.entity_ids('binary_sensor'):
     state = hass.states.get(entity_id)
-    if(state.object_id in ['family_home_mfc','family_home_aac','family_home_paf','family_home_aaf'] and state.state == 'on'):
+    if state.object_id in ['family_home_mfc','family_home_aac','family_home_paf','family_home_aaf'] and state.state == 'on':
         home = home + 1
 
 hass.states.set('sensor.family_home', home, {
     'unit_of_measurement': 'personas',
     'friendly_name': '¿Gente en Casa?'
+})
+
+sleep = 0
+for entity_id in hass.states.entity_ids('input_boolean'):
+    state = hass.states.get(entity_id)
+    if state.object_id in ['family_sleep_home_mfc','family_sleep_home_aac',
+                           'family_sleep_home_paf','family_sleep_home_aaf'] and state.state == 'on':
+        sleep = sleep + 1
+
+hass.states.set('sensor.family_sleep_home', sleep, {
+    'unit_of_measurement': 'personas',
+    'friendly_name': '¿Duermen en Casa?'
 })
